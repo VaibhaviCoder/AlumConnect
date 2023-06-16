aa3bb91bec11af1c7bae8a2793b3294355a13fb4
Vaibhavi Bhushan <97871717+VaibhaviCoder@users.noreply.github.com>
2023-06-16 14:56:39 +0530

    change some mistakes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => {
                     <h1 style={{ color:'white'}}>MIT, Muzaffarpur Alumni Community </h1>
                     <p>Welcome to our alumni community! This is your gateway to connect with us and over 10,000 former Mitian around the world</p>
                    <button className='homeBtn'> EXPLORE</button>
-                   <button className='homeBtn' onClick={onclickhandler}> SIG IN</button>
+                   <button className='homeBtn' onClick={onclickhandler}> SIGN IN</button>
                 </div>
             </div>
         </div>
